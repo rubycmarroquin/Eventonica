af0ee1745442be6185fa754fa58a1e4c13e2b6d4
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-14 19:10:36 -0700

    deleted(server.js): hardcoded data

--- server/server.js
+++ server/server.js
@@ -30,18 +30,6 @@ app.get("/events", async (req, res) => {
     console.log(error);
     return res.status(400).json({ error });
   }
-
-  // // hardcode the events response for testing reasons. This call has one more event that the real DB
-  // const events = [
-
-  //     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
-  //     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
-  //     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
-  //     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
-  //     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
-  //     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
-  //   ];
-  //   res.send(events);
 });
 
 app.get("/events/:eventName", async (req, res) => {
@@ -76,6 +64,7 @@ app.post("/events/", async (req, res) => {
 app.patch("/events/:id", async(req, res) => {
     const { title, location, eventtime } = req.body;
     const event_id = req.params.id;
+    
     db.query(
     `UPDATE events SET title = '${title}', location = '${location}', eventtime = '${eventtime}' WHERE id = ${event_id}`,
     (error, results) => {
