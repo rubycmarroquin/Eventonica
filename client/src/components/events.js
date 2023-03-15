660c65623bbee8edb8e589d49ae2fc02bc1e4f11
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-15 14:11:30 -0700

    moved effect to app.js

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -1,20 +1,9 @@
-import { useState, useEffect } from "react";
 import EventCard from "./event";
 import CardGroup from 'react-bootstrap/CardGroup';
 
 
 
-function Events() {
-    const [events, setEvents] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:8080/events")
-          .then((response) => response.json())
-          .then(events => {
-            setEvents(events); 
-            console.log('Events fetched...', events);
-            });
-          }, []);
+function Events({events}) {
 
   return (
     <CardGroup className="Events">
