a024b677af0220861bb6ff0f8dee1703f6aca034
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-15 07:42:39 -0700

    created basic structure for searchbar

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+const SearchBar = () => {
+    const onChange = () => {
+        
+    }
+
+    const loadEvents = () => {
+
+    }
+
+    const [searchInput, setSearchInput] = useState("");
+    <form>
+
+    </form>
+    return
+}
+
+export default SearchBar;
