f3aec9b713ec01a01e46949873930723510e2a2c
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-16 11:38:49 -0700

    added(event.js): function to validate date

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -45,3 +45,12 @@ const EventCard = (props) => {
 
 export default EventCard;
 
+
+
+function validateTitleLocation(title, location) {
+  if(title.length > 50 || title.length < 5) {
+    return "Title must be at least 5 characters";
+  } else if(location.length > 50 || location.length < 10) {
+    return "Location must be at least 10 characters";
+  } else return true;
+}
