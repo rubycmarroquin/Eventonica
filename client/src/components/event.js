fd07e6b15282fc19420bb48f995c730d1761ace6
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-16 11:39:11 -0700

    added(event.js): function to validate location and title

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -45,7 +45,26 @@ const EventCard = (props) => {
 
 export default EventCard;
 
-
+// validate if date format is correct 
+function validateDate(date) {
+  if(date.length !== 10) {
+    return "Date format incorrect";
+  }
+  else if(date[4] !== "-" && date[7] !== "-") {
+    return "Date format incorrect";
+  } else {
+    let year = +(date.substring(0,4));
+    let month = +(date.substring(5,7));
+    let day = +(date.substring(8,10));
+    if(typeof year !== "number" && typeof day !== "number" && typeof month !== "number") {
+      return "Enter a number for year, day, and month";
+    } else if(year >= 2023 && month >= 1 && month <= 12 && day >=1 && day <= 31) {
+      return true;
+    } else {
+      return "Not a valid date";
+    }
+}
+}
 
 function validateTitleLocation(title, location) {
   if(title.length > 50 || title.length < 5) {
