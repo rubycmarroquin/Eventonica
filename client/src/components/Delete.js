dd3ac78b50204629cfde753adc48ce6d6801e9ca
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-17 08:41:15 -0700

    added(Delete.js): password prompt in order to delete

--- client/src/components/Delete.js
+++ client/src/components/Delete.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+
 const DeleteButton = ({id}) => {
+    const [promptUser, setPromptUser] = useState(false);
+
     const handleDelete = () => {
         window.location.reload();
     };
+
+    const confirmation = () => {
+        let password = window.prompt("What is the password?");
+        if(password === 'ruby') {
+            setPromptUser(true);
+        } else {
+            alert("Incorrect password");
+        }
+    }
 
     async function deleteEvent() {
         await fetch("http://localhost:8080/events/" + id, {
@@ -15,7 +29,11 @@ const DeleteButton = ({id}) => {
 
     return (
         <div>
-            <button type="button" onClick={deleteEvent}>Delete</button>
+            {!promptUser ? (
+            <Button variant="danger" onClick={confirmation}>Delete</Button>
+            ) : (
+            <Button variant="danger" onClick={deleteEvent}>Confirm Delete</Button>
+            )}
         </div>
     )
 }
